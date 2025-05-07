1e47df193fc3eb6c7b76ac66a5c497aa5a9e747e
Tomas Novotny @hex_ontos <git.hex_ontos@tomas-novotny.slmail.me>
2025-05-07 03:31:20 +0200

    solved 27

---
+++ leetcode/27. Remove Element/solution.c
@@ -0,0 +1,12 @@
+// Time: O(n)
+// Space: O(1)
+int removeElement(int* nums, int numsSize, int val) {
+    int j = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+}
